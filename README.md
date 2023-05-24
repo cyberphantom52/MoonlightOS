2fa618454fde6e99ca3f5e7634a1e6c7f669e405
Cyber Phantom <inam123451@gmail.com>
2023-05-24 16:03:37 +0530

    Inial baremetal program

---
+++ README.md
@@ -0,0 +1,11 @@
+# Moonlight OS
+WIP
+
+## Build Instructions
+```
+rustup toolchain install nightly
+rustup override set nightly
+rustup component add llvm-tools
+cargo install bootimage
+cargo run
+```
---
+++ src/main.rs
@@ -0,0 +1,31 @@
+#![no_std] // don't link the Rust standard library
+#![no_main] // disable all Rust-level entry points
+
+use core::panic::PanicInfo;
+
+// Text to print to the vga buffer
+static HELLO: &[u8] = b"MoonlightOS!";
+
+#[no_mangle] // don't mangle the name of this function
+pub extern "C" fn _start() -> ! {
+    // this function is the entry point, since the linker looks for a function
+    // named `_start` by default
+
+    // https://os.phil-opp.com/vga-text-mode/
+    let vga_buffer = 0xb8000 as *mut u8;
+    
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
+    loop {}
+}
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
