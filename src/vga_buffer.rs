5e7faffe3837b6b37d3dbb6bfb59394014f1b213
Cyber Phantom <inam123451@gmail.com>
2023-05-27 19:27:13 +0530

    Implement proper VGA Text Mode for printing out to console

---
+++ src/vga_buffer.rs
@@ -0,0 +1,171 @@
+#[allow(dead_code)]
+#[repr(u8)]
+enum Color {
+    Black = 0,
+    Blue = 1,
+    Green = 2,
+    Cyan = 3,
+    Red = 4,
+    Magenta = 5,
+    Brown = 6,
+    LightGray = 7,
+    DarkGray = 8,
+    LightBlue = 9,
+    LightGreen = 10,
+    LightCyan = 11,
+    Pink = 12,
+    LightMagenta = 13,
+    Yellow = 14,
+    White = 15,
+}
+
+#[repr(transparent)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct ColorCode(u8);
+
+// Color bit of vga buffer uses MSB to determine if the char is blinking
+// The next three bits are used to set the background color
+// And the remaining 4 bits are used to set the foreground color
+impl ColorCode {
+    fn new(foreground: Color, background: Color) -> ColorCode {
+        ColorCode((background as u8) << 4 | (foreground as u8))
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(C)]
+// Each character in the vga buffer is of 2 bytes with the first being the char and 2nd being the byte
+struct ScreenChar {
+    ascii_char: u8,
+    color_code: ColorCode
+}
+
+// Standard size of VGA Buffer
+const BUFFER_HEIGHT: usize = 25;
+const BUFFER_WIDTH: usize = 80;
+
+// https://os.phil-opp.com/vga-text-mode/#volatile
+// Compiler doesn’t know that we really access VGA buffer memory (instead of normal RAM) and knows nothing about the side effect that some characters appear on the screen. 
+// So it might decide that these writes are unnecessary and can be omitted. 
+// Using volatile crate to avoid erroneous optimization of the Buffer
+use volatile::Volatile;
+#[repr(transparent)]
+struct Buffer {
+    chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT]
+}
+
+
+pub struct Writer {
+    column_position: usize,
+    color_code: ColorCode,
+    buffer: &'static mut Buffer
+}
+
+
+// https://os.phil-opp.com/vga-text-mode/#lazy-statics
+// Statics are initialized at compile time.
+// The problem here is that Rust is not able to convert raw pointers to references at compile time.
+// lazy_static! macro , instead of computing its value at compile time, initializes itself when accessed for the first time.
+
+// https://os.phil-opp.com/vga-text-mode/#spinlocks
+// Writer is immutable by default which is pretty useless.
+// A `static mut` can solve the problem its highly discouraged as it can lead to data races.
+// An alternative is to use a spinlock. 
+use lazy_static::lazy_static;
+use spin::Mutex;
+lazy_static! {
+    pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::Yellow, Color::Black),
+        // 0xb8000 MMIO address for vga buffer
+        // https://os.phil-opp.com/vga-text-mode/#the-vga-text-buffer
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+    });    
+}
+
+impl Writer {
+    pub fn write_byte(&mut self, byte: u8) {
+        match byte {
+            b'\n' => self.new_line(),
+            byte => {
+                if self.column_position >= BUFFER_WIDTH {
+                    self.new_line();
+                }
+
+                // Always print at the last line of buffer
+                let row = BUFFER_HEIGHT - 1;
+                let col = self.column_position;
+
+                let color_code = self.color_code;
+                self.buffer.chars[row][col].write(ScreenChar {
+                    ascii_char: byte,
+                    color_code,
+                });
+                
+                self.column_position += 1;
+            }
+        }
+    }
+
+    pub fn write_string(&mut self, s: &str) {
+        for byte in s.bytes() {
+            match byte {
+                // printable ASCII byte or newline
+                0x20..=0x7e | b'\n' => self.write_byte(byte),
+                // not part of printable ASCII range
+                _ => self.write_byte(0xfe),
+            }
+        }
+    }
+
+    fn new_line(&mut self) {
+        for row in 1..BUFFER_HEIGHT {
+            for col in 0..BUFFER_WIDTH {
+                let character = self.buffer.chars[row][col].read();
+                self.buffer.chars[row - 1][col].write(character);
+            }
+        }
+        self.clear_row(BUFFER_HEIGHT - 1);
+        self.column_position = 0;
+    }
+
+    fn clear_row(&mut self, row: usize) {
+        let blank = ScreenChar {
+            ascii_char: b' ',
+            color_code: self.color_code,
+        };
+
+        for col in 0..BUFFER_WIDTH {
+            self.buffer.chars[row][col].write(blank);
+        }
+    }
+}
+
+
+
+// Implemet fmt::Writer for our Writer so we can use rust's formatting macros like write!/writeln!
+use core::fmt;
+impl fmt::Write for Writer {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.write_string(s);
+        Ok(())
+    }
+}
+
+// Marco overrides for standard print macros so we can use them again.
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::vga_buffer::_print(format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
+}
+
+#[doc(hidden)]
+pub fn _print(args: fmt::Arguments) {
+    use core::fmt::Write;
+    WRITER.lock().write_fmt(args).unwrap();
+}
