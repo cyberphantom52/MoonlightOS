e5223b4e599b99eb4cb564abf7a1d66235b09499
muneeb-i-khan <muneebk015@gmail.com>
2023-09-10 19:47:07 +0530

    Added basic commands

--- src/shell/shell.rs
+++ src/shell/shell.rs
@@ -59,10 +59,111 @@ impl Shell {
         self.init();
     }
 
-    pub fn interpret(&mut self) {
-        let mut writer = WRITER.lock();
-        writer.write_string("Unknown command!");
-        writer.new_line();
-        drop(writer);
+    fn interpret(&mut self) {
+        match self.buffer {
+            _b if self.is_command("ping") => {
+                let mut writer = WRITER.lock();
+                writer.write_string("pong");
+                writer.new_line();
+                drop(writer);
+            }
+            _b if self.is_command("help") => {
+                let mut writer = WRITER.lock();
+                let border = "+-------------------------------------------+";
+
+                writer.write_string(border);
+                writer.new_line();
+                writer.write_string("| Available commands:                       |");
+                writer.new_line();
+                writer.write_string("| ping  --> prints pong                     |");
+                writer.new_line();
+                writer.write_string("| echo  --> prints any string               |");
+                writer.new_line();
+                writer.write_string("| help  --> lists available commands        |");
+                writer.new_line();
+                writer.write_string("| clear --> clears the screen               |");
+                writer.new_line();
+                writer.write_string("| osinfo --> prints OS information          |");
+                writer.new_line();
+                writer.write_string(border);
+                writer.new_line();
+                drop(writer);
+            }
+            _b if self.is_command("osinfo") => {
+                const OSINFO_ASCII_ART: &str = r#"
+                __  __ _                     _            -      -
+                |  \/  |                    | |          | |    | |
+                | \  / |  __   __    _ _ __ | |(_)_____  | |    | | _
+                | |\/| |/    \/   \ | '- \ \| || ||      | |___ |  __|
+                | |  | |     |     || |  | || || || ____ |  -- \|  |_
+                |_|  |_|\ ___/\___/ |_|  |_||_||_||_____||_|  |_|\_ _|
+"#;
+
+                let mut writer = WRITER.lock();
+                writer.set_colors(Color::Cyan, Color::Black);
+                writer.write_string(OSINFO_ASCII_ART);
+                writer.reset_colors();
+                writer.new_line();
+                writer.write_string("OS Name: MoonlightOS");
+                writer.new_line();
+                writer.write_string("OS Version: 1.0.0");
+                writer.new_line();
+                drop(writer);
+            },
+            _b if self.is_command("echo") => {
+                let mut message_started = false;
+                if self.buffer[self.cursor - 1] != '"' {
+                    let mut writer = WRITER.lock();
+                    writer.set_colors(Color::Pink, Color::Black);
+                    writer.write_string("Unknown command!");
+                    writer.reset_colors();
+                    writer.new_line();
+                    drop(writer);
+                    return;
+                }
+                for i in 0..self.cursor {
+                    let c = self.buffer[i];
+
+                    if c == '"' {
+                        if message_started {
+                            let mut writer = WRITER.lock();
+                            writer.new_line();
+                            drop(writer);
+                            break;
+                        } else {
+                            message_started = true;
+                        }
+                    } else if message_started {
+                        let mut writer = WRITER.lock();
+                        writer.write_char(c);
+                        drop(writer);
+                    }
+                }
+            },
+            _b if self.is_command("clear") => {
+                let mut writer = WRITER.lock();
+                writer.clear_screen();
+                drop(writer);
+            },
+            _ => {
+                let mut writer = WRITER.lock();
+                writer.set_colors(Color::Red, Color::Black);
+                writer.write_string("Unknown command!");
+                writer.reset_colors();
+                writer.new_line();
+                drop(writer);
+            }
+        }
+    }
+
+    fn is_command(&self, command: &str) -> bool {
+        let mut i = 0;
+        for c in command.chars() {
+            if c != self.buffer[i] {
+                return false;
+            }
+            i += 1;
+        }
+        true
     }
 }
